af614fb5663a001f9f9cb03a1b589c1bc651de33
bhirmbani <BhirawaMbani@Bhirawas-MacBook-Pro.local>
2016-07-27 17:08:46 +0700

    add carousel functionality

---
+++ js/script.js
@@ -0,0 +1,50 @@
+// carousel
+carousel = (function(){
+  var one = document.getElementById('one');
+  var two = document.getElementById('two');
+  var three = document.getElementById('three');
+  var box = document.querySelector('#carousel-box');
+  // var inner = document.querySelector('.content-inner');
+  var next = document.querySelector('#right');
+  var prev = document.querySelector('#left');
+  var items = box.querySelectorAll('section');
+  var counter = 0;
+  var amount = items.length;
+  var current = items[1];
+
+
+  function navigate(direction) {
+    current.classList.remove('active');
+    current.classList.add('content-header');
+    counter = counter + direction;
+    if(direction === -1 && counter < 0) {
+      counter = amount - 1;
+    }
+    else if(direction === 1 && !items[counter]) {
+      counter = 0;
+    }
+    current = items[counter];
+    current.classList.add('active');
+  }
+  next.addEventListener('click', function(ev){
+    navigate(1);
+      current.classList.remove('content-header');
+
+  });
+  prev.addEventListener('click', function(ev){
+    navigate(-1);
+    current.classList.remove('content-header');
+  });
+  navigate(0);
+})();
+
+
+
+
+
+// document.getElementById('one').style.background = 'url(images/header2.jpg)';
+
+
+// two.style.background = 'url(images/header2.jpg)';
+// two.classList.add('active');
+// three.style.background = 'url(images/header3.jpg)';
